e52e6ea9ddd51133f91b941a1f1a166820e5af11
judejay <judejay26@gmail.com>
2023-07-11 11:55:13 +0100

    invalid props

--- components/Input.tsx
+++ components/Input.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { TextInput, StyleSheet, TextInputProps } from 'react-native';
 
 const Input: React.FC<TextInputProps> = (props: TextInputProps) => {
-  return <TextInput {...props} style={{ ...styles.input, ...props }} />;
+  return <TextInput {...props} style={{ ...styles.input }} />;
 };
 
 const styles = StyleSheet.create({
